2c28fca4af67d57417252b6a036ffbe6731a9994
Wei Guo <qiangzheguo@gmail.com>
2023-10-20 12:59:58 -0400

    add about component

--- frontEnd/src/app/about/about.component.ts
+++ frontEnd/src/app/about/about.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {
-
+  head = '2023 My First Angular Project';
+  discription =
+    'This is my first Angular project. I will add more features to this project in the future.';
+  features = {
+    1: 'Connect front-end to back-end',
+    2: 'Handle wrong inputs',
+    3: 'Handle user authentication',
+  };
 }
